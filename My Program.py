e43f4ca986ef3e2f8e7ca68ee18aa3117f19ae88
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-09-11 20:24:31 +0500

    Add files via upload

---
+++ My Program.py
@@ -0,0 +1,37 @@
+# Text Data = String 
+# Examples :
+a="I can do whatever I want"
+b="This is my 6th class of Python Programming"
+print(a)
+print(b)
+
+# Boolean Data : True or False
+# Examples :
+a1=("True")
+b1=("False")
+print(a1)
+print(b1)
+
+# Numeric Data : Integer, Float, Complex
+# Examples :
+a2=(1)
+b2=(2.5)
+c2=(3 , 5)
+print(a2)
+print(b2)
+print(c2)
+
+# Sequence Data : list, Tuple
+# Examples :
+
+list = [22, 26, 307, ["orange", "banana", "tarbooz"]]
+print(list)
+
+tuple = (("sher", "daish", ), ("positivity", "negativity"))
+print(tuple)
+
+# Mapped Data : Dict
+# Examples :
+
+dict1 = {"name":"ismail", "age":18, "passion": "life"}
+print(dict1)
