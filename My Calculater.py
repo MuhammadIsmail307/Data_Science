21ce172a489aaaf5a6c3e1ada3b3c0d8c44ce387
MUHAMMAD ISMAIL <ismailexpert26307@gmail.com>
2024-09-14 20:48:31 +0500

    Add files via upload

---
+++ My Calculater.py
@@ -0,0 +1,10 @@
+x = 22
+y = 7
+z = x + y
+print("Addition of ", x, "and" , y, "is" , z)
+z = x-y
+print("Subtraction of ", x, "and" , y, "is" , z)
+z = x*y
+print("Multiplication of ", x, "and" , y, "is" , z)
+z = x/y
+print("Division of ", x, "and" , y, "is" , z)
